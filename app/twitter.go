cc21b4445f598441214c2650b53e71a4418f0fc9
utyosu <utyosu@gmail.com>
2021-03-01 15:39:46 +0000

    Twitterで表示される募集に人数を表示

--- app/twitter.go
+++ app/twitter.go
@@ -85,11 +85,11 @@ func buildTwitterMessage(twitterConfig *db.TwitterConfig, c *db.Channel, r *db.R
 	case TwitterTypeOpen, TwitterTypeUpdate:
 		memberNames := r.MemberNames()
 		if len(memberNames) > 0 {
-			return i18n.T(c.Language, "twitter_recruit", twitterConfig.Title, r.Title, r.AuthorName()) +
+			return i18n.T(c.Language, "twitter_recruit", twitterConfig.Title, r.Title, r.AuthorName(), len(r.Participants)-1, r.Capacity-1) +
 				"\n" +
 				i18n.T(c.Language, "twitter_members", strings.Join(memberNames, ", "))
 		} else {
-			return fmt.Sprintf("%v\n%v by %v", twitterConfig.Title, r.Title, r.AuthorName())
+			return i18n.T(c.Language, "twitter_recruit", twitterConfig.Title, r.Title, r.AuthorName(), len(r.Participants)-1, r.Capacity-1)
 		}
 	case TwitterTypeClose:
 		return i18n.T(c.Language, "twitter_close", twitterConfig.Title)
