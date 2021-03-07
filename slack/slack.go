7d8e91fee5750b04db2221ea6d215cd205e5a7f4
utyosu <utyosu@gmail.com>
2021-03-07 19:34:17 +0900

    slackで表示される標準出力を抑えめに変更

--- slack/slack.go
+++ slack/slack.go
@@ -14,7 +14,7 @@ type Config struct {
 }
 
 func (c *Config) Post(p ...interface{}) {
-	log.Printf("[%v]\n%v", c.Title, printString(true, p...))
+	log.Printf("[%v]\n%v", c.Title, printString(false, p...))
 	client := slack.New(c.Token)
 	c.postSlack(client, p...)
 }
@@ -23,7 +23,7 @@ func (c *Config) postSlack(client *slack.Client, p ...interface{}) {
 	_, err := client.UploadFile(
 		slack.FileUploadParameters{
 			Title:    c.Title,
-			Content:  printString(false, p...),
+			Content:  printString(true, p...),
 			Channels: []string{c.Channel},
 		},
 	)
@@ -32,16 +32,20 @@ func (c *Config) postSlack(client *slack.Client, p ...interface{}) {
 	}
 }
 
-func printString(enableColor bool, p ...interface{}) string {
+func printString(detail bool, p ...interface{}) string {
 	lpp := pp.New()
-	lpp.SetColoringEnabled(enableColor)
+	lpp.SetColoringEnabled(false)
 	var ret string
 	for _, v := range p {
 		if _, ok := v.(error); ok {
 			// errorはppで展開すると読めないのでfmtで表示する
 			ret += fmt.Sprintf("%+v\n", v)
 		} else {
-			ret += lpp.Sprintf("%v\n", v)
+			if detail {
+				ret += lpp.Sprintf("%v\n", v)
+			} else {
+				ret += fmt.Sprintf("%#v\n", v)
+			}
 		}
 	}
 	return ret
