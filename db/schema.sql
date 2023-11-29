a5a86fd4ea9d553ef7f7ee38a820eb8c94f0fd8d
utyosu <utyosu@gmail.com>
2023-11-29 21:04:32 +0900

    MySQL8.0のスキーマ定義にschema.sqlを合わせる

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -11,66 +11,66 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 DROP TABLE IF EXISTS `channels`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `channels` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_channel_id` bigint(20) NOT NULL,
-  `discord_guild_id` bigint(20) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `discord_channel_id` bigint NOT NULL,
+  `discord_guild_id` bigint NOT NULL,
   `recruitment` tinyint(1) NOT NULL,
   `timezone` varchar(30) NOT NULL,
   `language` varchar(10) NOT NULL DEFAULT '',
-  `reserve_limit_time` int(10) unsigned DEFAULT '0',
-  `expire_duration` int(10) unsigned DEFAULT '3600',
-  `expire_duration_for_reserve` int(10) unsigned DEFAULT '1800',
-  `twitter_config_id` int(10) unsigned NOT NULL DEFAULT '0',
+  `reserve_limit_time` int unsigned DEFAULT '0',
+  `expire_duration` int unsigned DEFAULT '3600',
+  `expire_duration_for_reserve` int unsigned DEFAULT '1800',
+  `twitter_config_id` int unsigned NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `nicknames`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `nicknames` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_user_id` bigint(20) NOT NULL DEFAULT '0',
-  `discord_guild_id` bigint(20) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `discord_user_id` bigint NOT NULL DEFAULT '0',
+  `discord_guild_id` bigint NOT NULL,
   `name` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_discord_user_id_and_discord_guild_id` (`discord_user_id`,`discord_guild_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `participants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `participants` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_user_id` bigint(20) NOT NULL DEFAULT '0',
-  `recruitment_id` int(10) unsigned NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `discord_user_id` bigint NOT NULL DEFAULT '0',
+  `recruitment_id` int unsigned NOT NULL,
   `alternate` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `recruitment_id` (`recruitment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `recruitments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recruitments` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_channel_id` bigint(20) NOT NULL DEFAULT '0',
-  `label` int(10) unsigned NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `discord_channel_id` bigint NOT NULL DEFAULT '0',
+  `label` int unsigned NOT NULL,
   `title` varchar(100) NOT NULL,
-  `capacity` int(10) unsigned NOT NULL,
+  `capacity` int unsigned NOT NULL,
   `active` tinyint(1) NOT NULL,
   `notified` tinyint(1) NOT NULL,
-  `tweet_id` bigint(20) NOT NULL DEFAULT '0',
+  `tweet_id` bigint NOT NULL DEFAULT '0',
   `reserve_at` timestamp NULL DEFAULT NULL,
   `expire_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -78,22 +78,22 @@ CREATE TABLE `recruitments` (
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `index_discord_channel_id_and_active` (`discord_channel_id`,`active`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `schema_migrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `schema_migrations` (
-  `version` bigint(20) NOT NULL,
+  `version` bigint NOT NULL,
   `dirty` tinyint(1) NOT NULL,
   PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `twitter_configs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `twitter_configs` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `consumer_key` varchar(100) NOT NULL,
   `consumer_secret` varchar(100) NOT NULL,
   `access_token` varchar(100) NOT NULL,
@@ -103,20 +103,20 @@ CREATE TABLE `twitter_configs` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `discord_user_id` bigint(20) NOT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `discord_user_id` bigint NOT NULL,
   `name` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
